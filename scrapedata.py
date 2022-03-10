249592bdec02aaa92cc7a2a49d4a2ea762812a9e
Vitelavita <68541254+Vitelavita@users.noreply.github.com>
2022-03-10 01:26:19 -0700

    Add files via upload

---
+++ scrapedata.py
@@ -0,0 +1,17 @@
+import urllib.request
+import re
+count = 0
+from bs4 import BeautifulSoup
+
+url = "http://python-data.dr-chuck.net/comments_283399.html"
+
+html = urllib.request.urlopen(url).read()
+soup = BeautifulSoup(html)
+
+tags = soup("span")
+
+for tag in tags:
+	stringnumber = tag.contents
+	number= int(stringnumber[0-1])
+	count = count+ number
+print (count)
